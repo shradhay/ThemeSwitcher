d8b8dfdb2ac2dd4d481d7b8c11d922f0d7fd78fd
shradhay <shraddhayannawar16@gmail.com>
2020-11-08 21:40:26 -0800

    Theme Switcher between  dark mode  and light mode

---
+++ src/colors.js
@@ -0,0 +1,16 @@
+const AppTheme={
+    light:{
+        textColor:'#4C4B4B',
+        backgroundColor:"#DAE0E2",
+        border:"2px solid #000"
+
+    },
+    dark:{
+        textColor:"#DAE0E2",
+        backgroundColor:"#4C4B4B",
+        bordor: "2px dashed #FFF",
+        }
+
+    
+}
+export default colors;
